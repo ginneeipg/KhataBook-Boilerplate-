b708455f8f97235b94a0a05d2088237f3b698d6c
Nitesh Bhagat <nitshbhgt1998@gmail.com>
2023-08-04 01:15:14 +0530

    major UI changes and Improvement

--- Components/CombBox/ComboBox.tsx
+++ Components/CombBox/ComboBox.tsx
@@ -21,27 +21,20 @@ import {
 } from "@/components/ui/popover";
 import { ScrollArea } from "../ui/scroll-area";
 import AddCategorySheet from "../SideSheet/AddCategorySheet";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-} from "../ui/dialog";
-import { DialogTrigger } from "@radix-ui/react-dialog";
 
 export function ComboboxDemo({
   list,
   value,
   setValue,
 }: {
-  list: string[];
-  value: string;
+  list: any[];
+  value: any;
   setValue: any;
 }) {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal={true}>
       <PopoverTrigger asChild>
         <Button
           variant="outline"
@@ -51,55 +44,63 @@ export function ComboboxDemo({
         >
           {value
             ? list.find(
-                (category: string) => category.toLocaleLowerCase() === value
-              )
-            : "Select framework..."}
+                (category: any) => category?.name.toLocaleLowerCase() === value
+              )?.name
+            : "Select category..."}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[21rem] p-0">
+      <PopoverContent className="w-[21rem] p-0 bg-white">
         <Command>
-          <CommandInput placeholder="Search framework..." />
+          <CommandInput placeholder="Search categories..." />
           <CommandEmpty className="h-28 flex flex-col justify-center items-center ">
-            <div className=" flex flex-col items-center ">
-              <span className="text-xl font-bold text-slate-400 ">
-                No Category found.
-              </span>
-              <span className="text-xs">No worries, just add a new one</span>
+            <div className=" flex flex-col items-stretch w-full p-2 ">
+              <div className="flex-col flex items-center justify-center">
+                <span className="text-xl font-bold text-slate-400 ">
+                  No Category found.
+                </span>
+                <span className="text-xs">No worries, just add a new one</span>
+              </div>
+
               <div className="my-2"></div>
-              <Dialog>
-                <DialogTrigger >
-                  <button>Add new category</button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>Add new Category</DialogHeader>
-                  
-                  <input type="text" placeholder="Category" />
-                  <DialogFooter>
-                    <button>Cancel</button>
-                    <button>Add</button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
+              <AddCategorySheet value={value} setValue={setValue}/>
             </div>
           </CommandEmpty>
           <CommandGroup>
-            <ScrollArea className="h-72">
-              {list.map((category: string) => (
+            <ScrollArea className="h-60" type="always">
+              {list.length <= 0 && (
+                <div className=" flex flex-col items-stretch w-full p-2 ">
+                  <div className="flex-col flex items-center justify-center">
+                    <span className="text-xl font-bold text-slate-400 ">
+                      No Category found.
+                    </span>
+                    <span className="text-xs">
+                      No worries, just add a new one
+                    </span>
+                  </div>
+
+                  <div className="my-2"></div>
+                  <AddCategorySheet value={value} setValue={setValue}/>
+                </div>
+              )}
+              {list.map((category: any) => (
                 <CommandItem
-                  key={category}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
+                  key={category?.id}
+                  onSelect={(currentValue: any) => {
+                    setValue(
+                      currentValue === value ? "" : currentValue
+                      // currentValue
+                    );
                     setOpen(false);
                   }}
                 >
                   <Check
                     className={cn(
                       "mr-2 h-4 w-4",
-                      value === category ? "opacity-100" : "opacity-0"
+                      value === category?.name ? "opacity-100" : "opacity-0"
                     )}
                   />
-                  {category}
+                  {category?.name}
                 </CommandItem>
               ))}
             </ScrollArea>
@@ -109,3 +110,37 @@ export function ComboboxDemo({
     </Popover>
   );
 }
+
+// <Dialog>
+{
+  /* <DialogTrigger>
+<button className="bg-green-600 px-4 py-2 text-white rounded-md w-full text-sm">
+  Add new category
+</button>
+</DialogTrigger>
+<DialogContent>
+<DialogHeader>Add new Category</DialogHeader>
+
+<form
+  onSubmit={(event: any) => {
+    event.preventDefault();
+    addCategory(event.target.category.value);
+    setOpen(!open);
+  }}
+>
+  <input
+    type="text"
+    name="category"
+    placeholder="Category name"
+    className="p-2 bg-slate-50 focus:ring-green-600 focus:ring-1 outline-none rounded-md"
+  />
+  {/** some inputs */
+}
+//   <button type="submit">Submit</button>
+// </form>
+
+// <DialogFooter>
+//   <Button onClick={() => {}}>Save changes</Button>
+// </DialogFooter>
+// </DialogContent>
+// </Dialog> */}
--- Components/SideSheet/AddCategorySheet.tsx
+++ Components/SideSheet/AddCategorySheet.tsx
@@ -1,21 +1,64 @@
-import { Sheet } from "lucide-react";
-import React from "react";
-import { SheetContent, SheetTrigger } from "../ui/sheet";
+import React, { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 
-function AddCategorySheet() {
+function AddCategorySheet({value, setValue}:{value:any, setValue:any}) {
+  const [category, setCategory] = useState<string>("");
+  const [open, setOpen] = useState(false);
+
+  const handleSubmit = (data: any) => {
+    fetch("http://localhost:4000/categories", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        // Handle the response data
+        console.log("PUT request successful", responseData);
+      })
+      .catch((error) => {
+        // Handle errors here
+        console.error("Error making PUT request:", error);
+      });
+
+    // console.log(JSON.stringify(category));
+
+    setOpen(!open);
+    setValue(data)
+  };
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger className="bg-green-600 w-full text-white rounded-md p-2">
         Add new category
       </SheetTrigger>
-      <SheetContent>
-        <div className="flex flex-col">
+      <SheetContent className="bg-white pt-10">
+        <div className="flex flex-col bg-white gap-3">
+          <span className="text-2xl font-semibold">
+            Add a new <br />{" "}
+            <span className="font-bold text-green-500 text-3xl">Category</span>{" "}
+          </span>
           <input
             type="text"
             placeholder="Add new category"
-            className="border"
+            onChange={(e: any) => setCategory(e.target.value)}
+            className="border p-2 rounded-lg focus:ring-1 ring-0 focus:ring-green-500 outline-none"
           />
-          <button className="bg-green-600 text-white">Add</button>
+          <button
+            onClick={() =>
+              handleSubmit({ name: category?.toLocaleLowerCase() })
+            }
+            className="bg-green-600 text-white p-2 rounded-lg  border-none"
+          >
+            Add
+          </button>
         </div>
       </SheetContent>
     </Sheet>
